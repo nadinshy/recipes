762c8f4c229be84ffdc0939759c73e894ff81c81
Nadia <nadinshy@gmail.com>
2022-10-24 14:29:42 -0500

    first commit

---
+++ src/MyRecipesComponent.js
@@ -0,0 +1,26 @@
+import checkmark from './checkmark.png'
+
+function MyRecipesComponent({label, image, calories, ingredients}) {
+    return(<div>
+    <div className='container'>
+        <h2>{label}</h2>
+        </div>
+        
+        <div className='container'>
+        <img className='tasty' src={image} alt='img'/>
+        </div>
+        
+    <ul className='list'>
+        {ingredients.map(ingredient => (
+            <li><img src={checkmark} alt='checkmark' 
+            className='icon'/>{ingredient}</li>
+        ))}
+    </ul>
+
+    <div className='container'>
+    <p className='par'>{calories.toFixed()} calories</p>
+    </div>
+    </div>
+)}
+
+export default MyRecipesComponent;
